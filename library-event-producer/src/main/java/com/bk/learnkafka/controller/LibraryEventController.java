042b25bb4c495fdd8061257c48c4ec6fd5450fed
Boonkuae Boonsutta <boonkuae.boo@ascendcorp.com>
2020-03-04 23:37:31 +0700

    Build LibraryEvents Producer using KafkaTemplate - Approach1- Part1

---
+++ library-event-producer/src/main/java/com/bk/learnkafka/controller/LibraryEventController.java
@@ -0,0 +1,22 @@
+package com.bk.learnkafka.controller;
+
+import com.bk.learnkafka.domain.LibraryEvent;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LibraryEventController {
+
+    @PostMapping("/v1/libraryevent")
+    public ResponseEntity<LibraryEvent> postLibraryEvent(
+            @RequestBody LibraryEvent libraryEvent
+    )
+    {
+        // Invoke kafka
+        return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
+    }
+}
